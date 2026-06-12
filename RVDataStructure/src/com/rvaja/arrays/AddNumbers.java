31c94b150bc7da59d8ec63237b77abbf1c409243
robinv <robinv@salesforce.com>
2026-06-11 22:50:40 -0700

    Added addNumbers method

--- RVDataStructure/src/com/rvaja/arrays/AddNumbers.java
+++ RVDataStructure/src/com/rvaja/arrays/AddNumbers.java
@@ -3,8 +3,18 @@ package com.rvaja.arrays;
 public class AddNumbers {
 
 	public static void main(String[] args) {
-		System.out.print("RV");
+		System.out.println("RV Addition of numbers in Arrays");
+		int[] arr = {1,2,3,4,5};
+		int sum = addNumbers(arr);		
+		System.out.println("Addition Result: "+ sum);
+	}
 
+	private static int addNumbers(int[] arr) {
+		int sum = 0;
+		for(int num:arr) {
+			sum +=  num;
+		}
+		return sum;
 	}
 
 }
